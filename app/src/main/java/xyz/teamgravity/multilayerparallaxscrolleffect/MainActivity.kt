8ae0b016a95e25eccff261f78c86947312ac2bda
Raheem <raheemideadev@gmail.com>
2021-10-22 20:22:34 +0500

    setting up resources
    Took 11 minutes

--- app/src/main/java/xyz/teamgravity/multilayerparallaxscrolleffect/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/multilayerparallaxscrolleffect/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.multilayerparallaxscrolleffect.ui.theme.MultiLayerParallaxScrollEffectTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
